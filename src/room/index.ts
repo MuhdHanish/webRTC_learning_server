96ca2dc57b63bafbfff42fa2d8753641181a0ddf
MuhdHanish <muhammedhanish11@gmail.com>
2023-12-17 21:10:25 +0530

    feat(roomId): created room id when creating a room and return to user

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,10 +1,13 @@
 import { Socket } from "socket.io";
+import { v4 as uuidV4 } from "uuid";
 
 export const roomHanlder = (socket: Socket) => {
     const userId = socket.id;
     const createRoom = () => {
         console.log(`Create a room requested by ${userId} 🟠`);
-        socket.emit(`created-room`);
+        const roomId = uuidV4();
+        socket.join(roomId);
+        socket.emit(`room-created`, { roomId });
         console.log(`Create room request accepted for ${userId} 🟢`);
     };
     socket.on(`create-room`, createRoom);
