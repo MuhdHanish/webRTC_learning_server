1f24c720927db0b18a50fc358f3c971c3e0aab89
MuhdHanish <muhammedhanish11@gmail.com>
2023-12-17 17:31:24 +0530

    feat(server): created server setup, and creatd instace for socket io and started subscired for events

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log(`it works`);
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const port = 8080;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on(`connnection`, () => {
+    console.log(`connnected`);
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on ${port}...🚀`);
+});
